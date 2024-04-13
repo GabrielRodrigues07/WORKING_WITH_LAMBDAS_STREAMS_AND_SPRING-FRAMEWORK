1303051d3da6b8e3e934311c00b8eb1e795f6eb6
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-04-13 11:19:47 -0300

    feat(m01-#2024-04-13) Consumindo dados de séries

--- src/main/java/br/com/alura/screematch/ScreematchApplication.java
+++ src/main/java/br/com/alura/screematch/ScreematchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screematch;
 
+import br.com.alura.screematch.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,10 @@ public class ScreematchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("teste");
+		var service = new ConsumoApi();
+
+		var json = service.obterDados("https://www.omdbapi.com/?apikey=afaa114e&t=supernatural");
+
+		System.out.println(json);
 	}
 }
