34a3cae61e4db0c5f0960a45264759147a4acf99
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-04-13 17:28:41 -0300

    feat(m01-#2024-04-13) Desserializando dados e Modelando dados da série

--- src/main/java/br/com/alura/screematch/ScreematchApplication.java
+++ src/main/java/br/com/alura/screematch/ScreematchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screematch;
 
+import br.com.alura.screematch.model.DadosSerie;
 import br.com.alura.screematch.service.ConsumoApi;
+import br.com.alura.screematch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,9 +17,12 @@ public class ScreematchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var service = new ConsumoApi();
+		var convert = new ConverteDados();
 
 		var json = service.obterDados("https://www.omdbapi.com/?apikey=afaa114e&t=supernatural");
 
-		System.out.println(json);
+		DadosSerie dadosSerie = convert.obterDados(json, DadosSerie.class);
+
+		System.out.println(dadosSerie);
 	}
 }
